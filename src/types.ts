0e9cc1c95ae3b4a00b5be1b1328a8f7f1045538d
RandQuran <siddiqmutarji.m@gmail.com>
2025-03-09 12:17:31 +0300

    modify question page

--- src/types.ts
+++ src/types.ts
@@ -2,17 +2,12 @@
 export interface Question {
   id: number;
   question: string;
-  fraction: string | null;
   money: number | null;
   case: string | null;
-  type: string | null;
-  order: number | null;
-  correctAnswerId?: number | null;
 }
 
 export interface Answer {
   id: number;
   question_id: number;
   answer: number;
-  is_correct: boolean;
 }
