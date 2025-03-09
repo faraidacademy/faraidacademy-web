4eb022fc5debd09501ea4caf19550a9e4cace9f4
RandQuran <siddiqmutarji.m@gmail.com>
2025-03-10 00:51:04 +0300

    header(before login change)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,24 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 
+import sitemap from '@astrojs/sitemap';
+
+import icon from 'astro-icon';
+
 export default defineConfig({
   image: {
     domains: ["googleusercontent.com"],
   },
+
   output: 'server',
+
   adapter: netlify({
     edgeMiddleware: false,
   }),
+
+  integrations: [sitemap(), icon({
+    include: {
+      "material-symbols": ["*"],
+    },
+  }),],
 });
